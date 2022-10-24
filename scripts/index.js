5c83d4ca53346bed620fcc9f9c9bdd1dddaf5349
Elena Liubimova <elena.liubimova3@gmail.com>
2022-10-24 23:54:16 +0300

    feat: реализовать кнопки перелистывания фото

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,36 @@
-const burgerButton = document.querySelector('.burger-button');
-const mobileMenuPopup = document.querySelector('.mobile-menu-popup');
-const pagesNavigation = document.querySelector('.pages-navigation').cloneNode(true);
-const socialMedia = document.querySelector('.social-media').cloneNode(true);
-const photoPopup = document.querySelector('.photo-popup');
-const photoPopupImage = document.querySelector('.photo-popup__image');
-const galleryImages = Array.from(document.querySelectorAll('.gallery__image'));
-const photoPopupCloseButton = document.querySelector('.photo-popup__close-button');
+const burgerButton = document.querySelector(".burger-button");
+const mobileMenuPopup = document.querySelector(".mobile-menu-popup");
+const pagesNavigation = document
+  .querySelector(".pages-navigation")
+  .cloneNode(true);
+const socialMedia = document.querySelector(".social-media").cloneNode(true);
+const photoPopup = document.querySelector(".photo-popup");
+const photoPopupImage = document.querySelector(".photo-popup__image");
+const galleryImages = Array.from(document.querySelectorAll(".gallery__image"));
+const photoPopupFlipButtonDirectionRight = document.querySelector(
+  ".photo-popup__flip-button_direction_right"
+);
+const photoPopupFlipButtonDirectionLeft = document.querySelector(
+  ".photo-popup__flip-button_direction_left"
+);
+let indexOfGalleryImage;
 
 function handleMobileMenu() {
-  mobileMenuPopup.classList.toggle('mobile-menu-popup_open');
+  mobileMenuPopup.classList.toggle("mobile-menu-popup_open");
 }
 
 function handleBurgerButton() {
-  burgerButton.classList.toggle('burger-button_active');
+  burgerButton.classList.toggle("burger-button_active");
 }
 
 function renderMobileMenu() {
   mobileMenuPopup.appendChild(pagesNavigation);
   mobileMenuPopup.appendChild(socialMedia);
-  pagesNavigation.classList.add('pages-navigation_mobile');
-  socialMedia.classList.add('social-media_mobile');
+  pagesNavigation.classList.add("pages-navigation_mobile");
+  socialMedia.classList.add("social-media_mobile");
 }
 
-burgerButton.addEventListener('click', () => {
+burgerButton.addEventListener("click", () => {
   handleBurgerButton();
   handleMobileMenu();
   renderMobileMenu();
@@ -31,22 +39,33 @@ burgerButton.addEventListener('click', () => {
 // Функция открытия попапа
 function openPopup(popup) {
   popup.classList.add(`photo-popup_opened`);
-  document.addEventListener('keydown', closePopupByEscapeButton);
+  document.addEventListener("keydown", closePopupByEscapeButton);
 }
 
 // Функция закрытия попапа
 function closePopup(popup) {
   popup.classList.remove(`photo-popup_opened`);
-  document.removeEventListener('keydown', closePopupByEscapeButton);
+  document.removeEventListener("keydown", closePopupByEscapeButton);
 }
 
 // Функция закрытия попапа кнопкой Escape
 function closePopupByEscapeButton(evt) {
-  if (evt.key === 'Escape') {
+  if (evt.key === "Escape") {
     const openedPopup = document.querySelector(`.photo-popup_opened`);
     closePopup(openedPopup);
   }
 }
+
+// Обработчик закрытия попапа кнопкой закрытия или кликом по оверлею
+
+photoPopup.addEventListener("mousedown", (evt) => {
+  if (
+    evt.target.classList.contains("photo-popup_opened") ||
+    evt.target.classList.contains("photo-popup__close-button")
+  ) {
+    closePopup(photoPopup);
+  }
+});
 
 //Функция открытия полноразмерного фото
 function openGalleryImage(image) {
@@ -55,13 +74,27 @@ function openGalleryImage(image) {
 
 //Слушатель нажатия на фото галереи
 galleryImages.forEach((galleryImage) => {
-  galleryImage.addEventListener('click', () => {
+  galleryImage.addEventListener("click", () => {
     openPopup(photoPopup);
     openGalleryImage(galleryImage);
+    indexOfGalleryImage = galleryImages.indexOf(galleryImage);
+    // if (indexOfGalleryImage === 0) {
+    //   photoPopupFlipButtonDirectionLeft.classList.add('photo-popup__flip-button_hidden');
+    // }
+    // if (indexOfGalleryImage === galleryImages.length - 1) {
+    //   photoPopupFlipButtonDirectionRight.classList.add('photo-popup__flip-button_hidden');
+    // }
   });
 });
 
-//Слушатель кнопки закрытия попапа
-photoPopupCloseButton.addEventListener('click', () => {
-  closePopup(photoPopup);
+// Обработчик кнопки "листать фото вправо"
+photoPopupFlipButtonDirectionRight.addEventListener("click", () => {
+  photoPopupImage.src = galleryImages[indexOfGalleryImage + 1].src;
+  indexOfGalleryImage = indexOfGalleryImage + 1;
+});
+
+// Обработчик кнопки "листать фото влево"
+photoPopupFlipButtonDirectionLeft.addEventListener("click", () => {
+  photoPopupImage.src = galleryImages[indexOfGalleryImage - 1].src;
+  indexOfGalleryImage = indexOfGalleryImage - 1;
 });
